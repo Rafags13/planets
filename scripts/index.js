b8450957cea562081329a720bb9d4bcb8d36c275
Rafael Veiga <rafael_todescato@hotmail.com>
2023-12-05 14:34:04 -0400

    <feat>: added system to require close menu when resize

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,40 @@
+let GLOBAL_NAVBAR_STATE_OPENED = false;
+
+function changeMenuIcon() {
+  const hamburguerMenu = document.querySelector('#hamburguer-menu .material-symbols-outlined.white.size-32');
+  hamburguerMenu.innerHTML = GLOBAL_NAVBAR_STATE_OPENED ? "close" : "menu";
+}
+
+function toggleSidenav() {
+  const hiddenMenu = document.querySelector('section.menu-overriding-page');
+  
+  hiddenMenu.classList.add(GLOBAL_NAVBAR_STATE_OPENED ? 'show' : 'hide');
+  hiddenMenu.classList.remove(GLOBAL_NAVBAR_STATE_OPENED ? 'hide' : 'show');
+}
+
+window.addEventListener('resize', (event) => {
+  const mobileSize = 390;
+  if (window.innerWidth > mobileSize && GLOBAL_NAVBAR_STATE_OPENED) {
+    GLOBAL_NAVBAR_STATE_OPENED = false;
+    toggleSidenav();
+    changeMenuIcon();
+  }
+});
+
 document.addEventListener('DOMContentLoaded', (event) => {
-  const currentRoute = window.location.pathname.replace('/pages/', '').replace('.html', '');
-  console.log(currentRoute)
+  const currentRoute = window.location.pathname.replace('/planets/', '').replace('.html', '');
+
+  var items = document.querySelectorAll('.list-item a');
+
+  for (var item of items) {
+    if (item.innerHTML.toLowerCase() === currentRoute) {
+      item.classList.add('background-ative');
+    }
+  }
 })
+
+function openMenu() {
+  GLOBAL_NAVBAR_STATE_OPENED = !GLOBAL_NAVBAR_STATE_OPENED;
+  toggleSidenav();
+  changeMenuIcon();
+}
