6d9d543af3f9888a60b02b7b32c30af93b1decc8
Rafael Veiga <rafael_todescato@hotmail.com>
2023-12-07 14:19:37 -0400

    <feat>: finished the static template for all planets and necessary animations

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,17 @@ import data from '../assets/data/database.json' assert { type: 'json' };
 
 window.openMenu = openMenu;
 
+const planetBorderColors = {
+    '/': '#6F2ED4',
+    'earth': '#6F2ED4',
+    'mercury': '#419EBB',
+    'venus': '#EDA249',
+    'mars': '#D83A34',
+    'jupiter': '#D86534',
+    'saturn': '#B87D43',
+    'uranus': '#1EC1A2',
+    'neptune': '#2D68F0'
+  }
 
 function currentTabs() {
   const tabs = document.querySelectorAll('.informations-mobile-tab .select-tab');
@@ -14,15 +25,16 @@ function currentTabs() {
 
 function selectTab(tabName) {
   const tabs = currentTabs();
+  const currentRoute = window.location.pathname.replace('/planet/', '').replace('.html', '');
 
   for (var tab of tabs) {
     if (tab.classList.contains('active')) {
-      console.log(tab)
       tab.classList.remove('active');
+      tab.removeAttribute('style', `border-bottom-color: ${planetBorderColors[currentRoute]}`);
     }
     if (tab.querySelector('.tab').innerHTML.toLowerCase().includes(tabName)) {
-      console.log(tab)
       tab.classList.add('active');
+      tab.setAttribute('style', `border-bottom-color: ${planetBorderColors[currentRoute]}`);
     }
   }
 }
@@ -37,14 +49,16 @@ window.selectDescriptionType = selectDescriptionType;
 
 function selectDescriptionType(typeName) {
   const buttons = currentDescriptionButtons();
+  const currentRoute = window.location.pathname.replace('/planet/', '').replace('.html', '');
 
   for (var button of buttons) {
     if (button.classList.contains('active-button')) {
       button.classList.remove('active-button');
+      button.removeAttribute('style');
     }
     if (button.querySelectorAll('.text-button')[1].innerHTML.toLowerCase().includes(typeName)) {
-      console.log(button.querySelectorAll('.text-button')[1].innerHTML.toLowerCase().includes(typeName))
       button.classList.add('active-button');
+      button.setAttribute('style', `background-color: ${planetBorderColors[currentRoute]}`);
     }
   }
 }
@@ -53,8 +67,19 @@ function selectDescriptionType(typeName) {
 window.selectTab = selectTab;
 
 window.addEventListener('DOMContentLoaded', (event) => {
-  const currentRoute = window.location.pathname.replace('./', '').replace('.html', '');
-  console.log(data);
-  currentTabs()[0].classList.add('active');
-  // TODO: Load informations here
-})
+  const currentRoute = window.location.pathname.replace('/planet/', '').replace('.html', '');
+
+  for (var tab of currentTabs()) {
+    if (tab.classList.contains('active')) {
+      tab.setAttribute('style', `border-bottom-color: ${planetBorderColors[currentRoute]}`);
+    }
+  }
+
+  for (var button of currentDescriptionButtons()) {
+    if (button.classList.contains('active-button')) {
+       button.setAttribute('style', `background-color: ${planetBorderColors[currentRoute]}`);
+    }
+  }
+
+
+});
