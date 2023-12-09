5bc8ddab877844bfbb09604e36305521242f88f6
Rafael Veiga <rafael_todescato@hotmail.com>
2023-12-09 17:03:13 -0400

    <feat>: added system to implement the star movement

--- scripts/index.js
+++ scripts/index.js
@@ -4,18 +4,28 @@ import { openMenu } from '../modules/navbarState.js';
 import data from '../assets/data/database.json' assert { type: 'json' };
 
 window.openMenu = openMenu;
+window.selectDescriptionType = selectDescriptionType;
+window.selectTab = selectTab;
 
 const planetBorderColors = {
-    '/': '#6F2ED4',
-    'earth': '#6F2ED4',
-    'mercury': '#419EBB',
-    'venus': '#EDA249',
-    'mars': '#D83A34',
-    'jupiter': '#D86534',
-    'saturn': '#B87D43',
-    'uranus': '#1EC1A2',
-    'neptune': '#2D68F0'
-  }
+  '/': '#6F2ED4',
+    '/earth': '#6F2ED4',
+    '/mercury': '#419EBB',
+    '/venus': '#EDA249',
+    '/mars': '#D83A34',
+    '/jupiter': '#D86534',
+    '/saturn': '#B87D43',
+    '/uranus': '#1EC1A2',
+    '/neptune': '#2D68F0'
+}
+
+const getCurrentRoute = () => {
+  const lastOcurrenceOfPath = window.location.pathname.lastIndexOf('/');
+
+  const currentRouteSubtring = window.location.pathname.substring(lastOcurrenceOfPath, window.location.pathname.length).replace('.html', '').replace('/index', '/');
+
+  return currentRouteSubtring;
+}
 
 function currentTabs() {
   const tabs = document.querySelectorAll('.informations-mobile-tab .select-tab');
@@ -25,7 +35,10 @@ function currentTabs() {
 
 function selectTab(tabName) {
   const tabs = currentTabs();
-  const currentRoute = window.location.pathname.replace('/planet/', '').replace('.html', '');
+
+  const currentRoute = getCurrentRoute();
+
+  setImageByParam(tabName);
 
   for (var tab of tabs) {
     if (tab.classList.contains('active')) {
@@ -39,17 +52,33 @@ function selectTab(tabName) {
   }
 }
 
+const setImageByParam = (param) => {
+  let image = document.querySelector('.image-container img');
+  
+  let currentPlanet = getCurrentRoute().replace('/index', '').replace('/', '');
+  currentPlanet = currentPlanet === '' ? 'earth' : currentPlanet;
+
+  switch (param) {
+    case 'overview':
+      image.src = `../assets/images/planet-${currentPlanet}.svg`;
+      break;
+    case 'structure':
+      image.src = `../assets/images/internal/planet-${currentPlanet}-internal.svg`;
+      break;
+  }
+
+}
+
 function currentDescriptionButtons() {
   const buttons = document.querySelectorAll('.change-informations-container .information-button');
 
   return buttons;
 }
 
-window.selectDescriptionType = selectDescriptionType;
-
 function selectDescriptionType(typeName) {
   const buttons = currentDescriptionButtons();
-  const currentRoute = window.location.pathname.replace('/planet/', '').replace('.html', '');
+  const currentRoute = getCurrentRoute();
+  setImageByParam(typeName);
 
   for (var button of buttons) {
     if (button.classList.contains('active-button')) {
@@ -63,23 +92,102 @@ function selectDescriptionType(typeName) {
   }
 }
 
+function setCanvasSize() {
+  var canvas = document.getElementById('stars-background');
+  const documentWidth = window.outerWidth;
+  var bodyHeight = document.body.scrollHeight;
 
-window.selectTab = selectTab;
+  canvas.height = bodyHeight;
+  canvas.width = documentWidth;
+}
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  const currentRoute = window.location.pathname.replace('/planet/', '').replace('.html', '');
+function quadrantsOfScreen() {
+  const gap = 10;
+  const documentWidth = window.outerWidth;
+  var bodyHeight = document.body.scrollHeight;
 
-  for (var tab of currentTabs()) {
-    if (tab.classList.contains('active')) {
-      tab.setAttribute('style', `border-bottom-color: ${planetBorderColors[currentRoute]}`);
-    }
+  const firstPoint = { width: documentWidth / 2 - gap, height: bodyHeight / 2 - gap };
+  const secondPoint = { width: documentWidth / 2 + gap, height: bodyHeight / 2 - gap };
+  const thirdPoint = { width: documentWidth / 2 - gap, height: bodyHeight / 2 + gap };
+  const fourthPoint = { width: documentWidth / 2 + gap, height: bodyHeight / 2 + gap };
+
+  return {firstPoint, secondPoint, thirdPoint, fourthPoint};
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  const currentRoute = getCurrentRoute();
+
+  currentTabs()[0].setAttribute('style', `border-bottom-color: ${planetBorderColors[currentRoute]}`);
+  currentDescriptionButtons()[0].setAttribute('style', `background-color: ${planetBorderColors[currentRoute]}`);
+
+  setCanvasSize();
+  createStar();
+
+  // const { firstPoint, secondPoint, thirdPoint, fourthPoint } = quadrantsOfScreen();
+});
+
+function returnCurrentContextFromCanvas() {
+  var canvas = document.getElementById('stars-background');
+  const ctx = canvas.getContext("2d");
+
+  return ctx;
+}
+
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
+
+function calculatePositionByInitialStarPosition(positionX, positionY) {
+  var bodyHeight = document.body.scrollHeight;
+  var spareHeight = bodyHeight - positionY;
+  var spareHypotenuse = Math.sqrt((positionX * positionX) + (spareHeight * spareHeight));
+  var realHypotenuse = (positionY * spareHypotenuse) / spareHeight;
+  var realWidth = (positionX * realHypotenuse) / spareHypotenuse;  
+  var valueToSumInX = realWidth / realHypotenuse;
+  var valueToSumInY = positionY / realHypotenuse;
+  
+  return {valueToSumInX, valueToSumInY};
+}
+class Star {
+  constructor(positionX, positionY, zIndex) {
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.zIndex = zIndex;
   }
 
-  for (var button of currentDescriptionButtons()) {
-    if (button.classList.contains('active-button')) {
-       button.setAttribute('style', `background-color: ${planetBorderColors[currentRoute]}`);
-    }
+  createStar() {
+    // console.log('teste')
+    const context = returnCurrentContextFromCanvas();
+    const documentWidth = window.innerWidth;
+    const documentHeight = document.body.scrollHeight;
+    let {valueToSumInX, valueToSumInY} = calculatePositionByInitialStarPosition(this.positionX, this.positionY);
+    this.positionX += valueToSumInX;
+    this.positionY -= valueToSumInY;
+    this.zIndex += 0.015;
+    context.clearRect(0, 0, documentWidth, documentHeight);
+    context.beginPath();
+    context.fillStyle = "white";
+    context.arc(this.positionX, this.positionY, this.zIndex, 0, 2 * Math.PI);
+    context.fill();
+    context.closePath();
+
+    // requestAnimationFrame(this.createStar);
+    // If you uncomment this above live, you'll see the hell inside your cpu
+    // good luck
   }
+}
 
 
+function createStar() {
+  let starOne = new Star(1000, 768, 5);
+
+  
+  starOne.createStar();
+  
+}
+window.addEventListener('resize', () => {
+  setCanvasSize();
 });
